31b917e15631ad50a4ea8a30716fcbcd71d6dd17
Gamer100Dev <115351738+Gamer100Dev@users.noreply.github.com>
2024-02-18 23:53:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,10 @@ install() {
     printf "Installing...\n"
     SCRIPT_DIR="$(dirname "$0")"
     SCRIPTS_DIR="$SCRIPT_DIR/Scripts"
-    cd "$SCRIPTS_DIR"
-    
-    # Compile C++ files
+    cd "$SCRIPTS_DIR" || exit
+
     g++ -std=c++20 MainScript.cpp SharedFunctions.cpp -o DownloadHandler -lcurl
-    wait
+    wait  # Wait for the compilation to finish
 
     INSTALL_DIR="$HOME/.local"
 
@@ -20,7 +19,9 @@ install() {
     mkdir -p "$INSTALL_DIR/bin"
     mkdir -p "$INSTALL_DIR/share/applications"
 
-    cp -rv "$SCRIPT_DIR"/* "$INSTALL_DIR/share/discord-bsd"
+    # Copy all files excluding unwanted ones
+    rsync -av --exclude='SharedFunctions.h' --exclude='SharedFunctions.cpp' --exclude='MainScript.cpp' "$SCRIPT_DIR/" "$INSTALL_DIR/share/discord-bsd"
+
     mv -v "$INSTALL_DIR/share/discord-bsd/discord.desktop" "$INSTALL_DIR/share/applications/"
     mv -v "$INSTALL_DIR/share/discord-bsd/discord" "$INSTALL_DIR/bin"
 
