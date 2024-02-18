9b1eb4f7c21957531cb4aabd04cefc7a5f989a28
Gamer100Dev <115351738+Gamer100Dev@users.noreply.github.com>
2024-02-18 23:24:28 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
+
 printf "Discord 0.0.25\n"
 printf "Discord desktop client based on Discord Web for FreeBSD\n"
 printf "\nDo not run this in root! Or else this script breaks and will replicate your current chdir! It will not install under root!\n"
 
 install() {
     printf "Installing...\n"
-    cd "$(dirname "$0")" || exit
-    mkdir -p ~/.local/share/discord-bsd
-    mkdir -p ~/.local/bin
-    mkdir -p ~/.local/share/applications
-    cp -rv ./* ~/.local/share/discord-bsd
-    mv -v ~/.local/share/discord-bsd/discord.desktop ~/.local/share/applications/
-    mv -v ~/.local/share/discord-bsd/discord ~/.local/bin
-    sed -i '' "s/Exec=auto1/Exec=\/home\/$USER\/.local\/bin\/discord/" ~/.local/share/applications/discord.desktop
-    sed -i '' "s/Icon=auto2/Icon=\/home\/$USER\/.local\/share\/discord-bsd\/discord.png/" ~/.local/share/applications/discord.desktop
-    cd /home/$USER/.local/share/discord-bsd
+    SCRIPT_DIR="$(dirname "$0")"
+    INSTALL_DIR="$HOME/.local"
+
+    mkdir -p "$INSTALL_DIR/share/discord-bsd"
+    mkdir -p "$INSTALL_DIR/bin"
+    mkdir -p "$INSTALL_DIR/share/applications"
+
+    cp -rv "$SCRIPT_DIR"/* "$INSTALL_DIR/share/discord-bsd"
+    mv -v "$INSTALL_DIR/share/discord-bsd/discord.desktop" "$INSTALL_DIR/share/applications/"
+    mv -v "$INSTALL_DIR/share/discord-bsd/discord" "$INSTALL_DIR/bin"
+
+    sed -i '' "s|Exec=auto1|Exec=$HOME/.local/bin/discord|" "$INSTALL_DIR/share/applications/discord.desktop"
+    sed -i '' "s|Icon=auto2|Icon=$HOME/.local/share/discord-bsd/discord.png|" "$INSTALL_DIR/share/applications/discord.desktop"
+
+    cd "$INSTALL_DIR/share/discord-bsd" || exit
     npm install
+
     printf "Discord 0.0.25 installed!\n"
     printf "\nSetting executable perms!"
-    chmod +x ~/.local/bin/discord
+    chmod +x "$INSTALL_DIR/bin/discord"
     printf "\nDiscord is ready to launch and use! Thank you for using this port! \n"
 }
 
